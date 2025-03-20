12a21a12b71f15a78453c67f3f65c5bef0274f80
higurashi88 <msippc1997@gmail.com>
2025-03-20 19:57:18 +0900

    デプロイ時に処理を挟む

--- djangotestapp/testapp/apps.py
+++ djangotestapp/testapp/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class TestappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'testapp'
+
+    def ready(self):
+        # アプリケーションの初期化処理
+        print("アプリケーションの初期化処理を実行します")
+        # ここに一度だけ実行したい処理を記述する
